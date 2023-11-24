ac9138d4c6cee3756dc64eefae416d8beb8c0a9b
TopKingHao <126229696+TopKingHao@users.noreply.github.com>
2023-11-24 17:09:56 +0800

    Update email.py

--- email.py
+++ email.py
@@ -4,36 +4,39 @@ import smtplib
 from email.mime.text import MIMEText
 # 构建邮件头
 from email.header import Header
-def send_email(msg:str):
-    # 发信方的信息，这里用google邮箱发送吧，然后接受是学校的asu邮箱
+def send_email(msg):
+    # 发信方的信息，这里用google邮箱发送吧
     # 这里需要填入你的google邮箱地址和密码
-    from_addr = 'yyyy@gemailcom.'
-    password = '****'
+    from_addr = 'hzhen3@asu.edu'
+    password = 'cebtvniaeynkodkx'
     # 收信方邮箱
-    to_addr = 'yyyy@asu.edu'
+    #to_addr = 'xxx@asu.edu  他的邮箱'
+    to_addr = '2752604849@qq.com'
     # 发信服务器
-    smtp_server = 'smtp.gemail.com'
+    smtp_server = 'smtp.gmail.com'
 
     # 邮箱正文内容，第一个参数为内容，第二个参数为格式(plain 为纯文本)，第三个参数为编码
     msg = MIMEText(msg, 'plain', 'utf-8')
     # 邮件头信息
-    msg['From'] = Header('Hao Zhen')  # 发送者
-    msg['To'] = Header('haozhen')  # 接收者
+    msg['From'] = Header('Hao Zhen ')  # 发送者
+    msg['To'] = Header('ryanryan8731')  # 接收者
     subject = 'warning_messgae'
     msg['Subject'] = Header(subject, 'utf-8')  # 邮件主题
 
     try:
         smtpobj = smtplib.SMTP_SSL(smtp_server)
         # 建立连接
-        smtpobj.connect(smtp_server, 587)    
+        smtpobj.connect(smtp_server, 465)    
         # 登录
         smtpobj.login(from_addr, password)   
         # 发送邮件
         smtpobj.sendmail(from_addr, to_addr, msg.as_string()) 
         print("email send successful")
-    except smtplib.SMTPException:
+    except smtplib.SMTPException as e:
         print("email send failed")
+        print(e)
     finally:
         # 关闭服务器
         smtpobj.quit()
 
+send_email('')
